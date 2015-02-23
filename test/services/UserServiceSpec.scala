12a1502223d9355f382a489bdf2004c5aa637a75
Kristian Widjaja <kwidjaja1312@gmail.com>
2015-02-23 15:33:40 +0800

    Update UserServiceSpec.scala
    Added test data

--- test/services/UserServiceSpec.scala
+++ test/services/UserServiceSpec.scala
@@ -16,6 +16,14 @@ import scala.concurrent.duration._
  */
 class UserServiceSpec extends BaseSpec {
 
+  /* Test Data */
+ /*{
+    "_id" : ObjectId("54e95e4ab221b8eac5e420e9"),
+    "email" : "john.doe@gmail.com",
+    "secret" : "password",
+    "registrationDate" : "2015-02-22T13:52:00.123"
+ }*/
+
   // All spec classes must invoke this method to start the fake application
   start()
 
